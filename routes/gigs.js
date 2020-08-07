d02f768a358976d77b751d63c177dbda9441323f
Kingluddite <howley.phil@gmail.com>
2020-08-07 18:55:55 -0400

    Add working form

--- routes/gigs.js
+++ routes/gigs.js
@@ -20,38 +20,37 @@ router.get('/', (req, res) =>
 router.get('/add', (req, res) => res.render('add'));
 
 // Add a gig
-// we change this from get to post
 router.post('/add', (req, res) => {
   let { title, technologies, budget, description, contact_email } = req.body; // eslint-disable-line camelcase
-
   const errors = [];
 
   // Validate Fields
-  if(!title) {
+  if (!title) {
     errors.push({ text: 'Please add a title' });
   }
-  if(!technologies) {
+  if (!technologies) {
     errors.push({ text: 'Please add some technologies' });
   }
-  if(!description) {
+  if (!description) {
     errors.push({ text: 'Please add a description' });
   }
-  if(!contact_email) {
-    errors.push({ text: 'Please add a contact email' });
-  }
+
+  // if (!contact_email) {
+  //   errors.push({ text: 'Please add a contact email' });
+  // }
 
   // Check for errors
-  if(errors.length > 0) {
+  if (errors.length > 0) {
     res.render('add', {
       errors,
       title,
       technologies,
       budget,
       description,
-      contact_email
+      contact_email,
     });
   } else {
-    if(!budget) {
+    if (!budget) {
       budget = 'Unknown';
     } else {
       budget = `$${budget}`;
@@ -59,16 +58,19 @@ router.post('/add', (req, res) => {
 
     // Make lowercase and remove space after comma
     technologies = technologies.toLowerCase().replace(/,[ ]+/g, ',');
-  // Insert into gigs table
-  db.Gig.create({
-    title,
-    technologies,
-    description,
-    budget,
-    contact_email,
-  })
-    .then(gig => res.redirect('/gigs'))
-    .catch(err => console.log(err));
+
+    // Insert into table
+    db.Gig.create({
+      title,
+      technologies,
+      description,
+      budget,
+      contact_email,
+    })
+      .then(gig => res.redirect('/gigs'))
+      .catch(err => res.render('error', { error: err.message }));
+  }
 });
+
 // don't forget to export it or the error won't go away
 module.exports = router;
